ea147b94e78dba7c53916d7196dfad2044ced4b4
sashaumnykh <st068332@student.spbu.ru>
2018-09-24 16:41:11 +0300

    Revert "task1"
    This reverts commit 4dcde1e35ba0f38b47a166f5c43641471ea6a49d.

--- task3.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-minus_n= -1 * n
-minimum = min(n, minus_n)
-maximum = max(n, minus_n)
-for i in range (minimum, maximum+1):
-    if i==0:
-        continue
-    if n%i==0:
-        print (i)
