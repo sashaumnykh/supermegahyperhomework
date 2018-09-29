4a022db15a67130372caef2060339ab4ace9a1b4
sashaumnykh <st068332@student.spbu.ru>
2018-09-29 09:46:09 +0300

    readme_task5_умных_101

--- task5.py
+++ task5.py
@@ -2,10 +2,9 @@ line = str(input())
 lenght_of_line = len(line)
 counter = 0
 for i in range (0, lenght_of_line//2):
-    if line[i]==line[lenght_of_line-1-i]:
-        continue
-    else:
-        counter = counter + 1
+    while counter==0:
+        if line[i]!=line[lengаht_of_line-1-i]:
+            counter = counter + 1
 if counter==0:
     print("Строка является палиндромом")
 else:
