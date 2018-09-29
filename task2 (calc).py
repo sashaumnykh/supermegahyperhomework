5ba3b7e60e6da3f040df440a9303c561a4890aed
sashaumnykh <st068332@student.spbu.ru>
2018-09-29 10:18:56 +0300

    readme_task2_умных_101

--- task2 (calc).py
+++ task2 (calc).py
@@ -1,17 +1,18 @@
 def calc(a, operation, b):
-    try:
-        if operation == '+':
-            return int(a) + int(b)
-        elif operation == '-':
-            return int(a) - int(b)
-        elif operation == '*':
-            return int(a) * int(b)
-        elif operation == '/' and int(b)!=0:
-            return int(a) / int(b)
+
+    if operation == '+':
+        return int(a) + int(b)
+    elif operation == '-':
+        return int(a) - int(b)
+    elif operation == '*':
+        return int(a) * int(b)
+    elif operation == '/':
+        if int(b)==0:
+            raise ZeroDivisionError("На ноль делить нельзя!!!")
         else:
-            return "Неизвестная операция"
-    except:
-        return "Неизвестная операция"
+            return int(a) / int(b)
+    else:
+        raise ValueError("Неизвестная операция")
 
 test = list(input().split(" "))
 print(calc(test[0],test[1],test[2]))
