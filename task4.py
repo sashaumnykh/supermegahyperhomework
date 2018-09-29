2248f671ed646d54c9663a2634b6bcc2d4a0d1cd
sashaumnykh <st068332@student.spbu.ru>
2018-09-29 09:52:54 +0300

    readme_task4_умных_101

--- task4.py
+++ task4.py
@@ -2,13 +2,13 @@ n = int(input())
 minus_n=-1*n
 minimum = min (n, minus_n)
 maximum = max (n, minus_n)
-summ_of_devisors=0
+number_of_devisors=0
 for i in range (minimum, maximum+1):
     if i==0:
         continue
     if n%i==0:
-        summ_of_devisors=summ_of_devisors+1
-if summ_of_devisors==4:
+        number_of_devisors=number_of_devisors+1
+if number_of_devisors==4:
     print("Число является простым")
 else:
     print("Число не является простым")
