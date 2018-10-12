cf0a7166abf9866fd37ca2ec04d1b3a6cf6809cf
sashaumnykh <st068332@student.spbu.ru>
2018-10-12 18:37:23 +0300

    READ_ME_06.10-13.10 task 1

--- 12oct_task1_fact recursion.py
+++ 12oct_task1_fact recursion.py
@@ -1,11 +1,18 @@
+import time
+from memory_profiler import profile
+@profile
 def factorial(n):
+    if type(n)!= int:
+        return "Нужно ввести целое число!"
     if n<0:
         raise RecursionError ("Нужно ввести неотрицательное число!")
+    elif n==0:
+        return 1
     else:
-        if n==0:
-            return 1
-        else:
-            return n*factorial(n-1)
+        return n*factorial(n-1)
 
+start_time = time.time()
+#factorial(5)
 test = int(input())
 print (factorial(test))
+print(time.time() - start_time)
--- 12oct_task1_fact.py
+++ 12oct_task1_fact.py
@@ -1,13 +1,22 @@
+import time
+from memory_profiler import profile
+@profile
 def factorial(n):
     fact = 1
-    if n==0:
+    if type(n)!=int:
+        return "n должно быть целым числом"
+    elif n==0:
         return 1
+    elif n<0:
+        return "n не может быть меньше 0"
     else:
         for i in range (1, n+1):
             fact=fact*i
-    return fact
+        return fact
 
+start_time = time.time()
+#factorial(5)
 test = int(input())
 print (factorial(test))
+print(time.time() - start_time)
 
-# придумай решение при n<0
