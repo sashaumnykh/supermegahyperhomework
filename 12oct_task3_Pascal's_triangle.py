15de5a729038741535a1cef7f71179731b36d716
sashaumnykh <st068332@student.spbu.ru>
2018-10-13 09:52:49 +0300

    READ_ME_task3_pascal's_triangle

--- 12oct_task3_Pascal's_triangle.py
+++ 12oct_task3_Pascal's_triangle.py
@@ -1,3 +1,4 @@
+import unittest
 from math import factorial
 def pascal(n):
     a=list()
@@ -11,3 +12,14 @@ n = int(input())
 z = pascal(n)
 for i in range (n):
     print (z[i])
+
+class pascal_test(unittest.TestCase):
+    def test_values(self):
+        self.assertEqual(pascal(1),[[1]])
+    def test_values1(self):
+        self.assertEqual(pascal(3),[[1], [1, 1], [1, 2, 1]])
+    def test_values2(self):
+        self.assertEqual(pascal(5),[[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]])
+
+if __name__ == "__main__":
+    unittest.main()
