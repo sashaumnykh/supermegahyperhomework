093dbcd317d61bcd80569d35ba915056ae6f4a2a
Aleksandra Umnykh <43292102+sashaumnykh@users.noreply.github.com>
2018-10-19 23:20:51 +0300

    READ_ME_task1_updated

--- 2_nd_homework/12oct_task1_fact recursion.py
+++ 2_nd_homework/12oct_task1_fact recursion.py
@@ -4,17 +4,16 @@ from memory_profiler import profile
 @profile
 def factorial(n):
     if type(n)!= int:
-        raise TypeError ("Нужно ввести число!")
-    else: n=int(n)
-    if n<0:
-        raise RecursionError ("Нужно ввести неотрицательное число!")
+        raise TypeError ("Аргумент должен быть целым числом!")
+    elif n<0:
+        raise ValueError ("Аргумент должен быть неотрицательным целым числом!")
     elif n==0:
         return 1
     else:
         return n*factorial(n-1)
 
 start_time = time.time()
-#factorial(5)
+factorial(5)
 #test = input()
 #print (factorial(test))
 print(time.time() - start_time)
@@ -22,16 +21,21 @@ print(time.time() - start_time)
 class factorial_test(unittest.TestCase):
     def test_5(self):
         self.assertEqual(factorial(5),120)
+    def test_1(self):
+        self.assertEqual(factorial(1), 1)
+    def test_12(self):
+        self.assertEqual(factorial(12), 479001600)
     def test_0(self):
         self.assertEqual(factorial(0),1)
     def test_3(self):
         self.assertEqual(factorial(3),6)
-    def test_falsetype(self):
-        self.assertRaises(factorial('vfgb'), TypeError)
+    def test_falsetype_str1(self):
+        self.assertRaises(TypeError, factorial, 'oooo')
+    def test_falsetype_str2(self):
+        self.assertRaises(TypeError, factorial, ' ')
+    def test_falsetype_negative(self):
+        self.assertRaises(ValueError, factorial, -15)
 
 if __name__ == "__main__":
     unittest.main()
-<<<<<<< HEAD:12oct_task1_fact recursion.py
-=======
-    
->>>>>>> 13582e6bb411575b1d230b579bf4f646c0e4f549:2_nd_homework/12oct_task1_fact recursion.py
+
--- 2_nd_homework/12oct_task1_fact.py
+++ 2_nd_homework/12oct_task1_fact.py
@@ -5,11 +5,11 @@ from memory_profiler import profile
 def factorial(n):
     fact = 1
     if type(n)!=int:
-        return "n должно быть целым числом"
+        raise TypeError ("Аргумент должен быть целым числом!")
     elif n==0:
         return 1
     elif n<0:
-        return "n не может быть меньше 0"
+        raise ValueError ("Аргумент должен быть неотрицательным целым числом!")
     else:
         for i in range (1, n+1):
             fact=fact*i
@@ -25,10 +25,20 @@ print(time.time() - start_time)
 class factorial_test(unittest.TestCase):
     def test_5(self):
         self.assertEqual(factorial(5),120)
+    def test_1(self):
+        self.assertEqual(factorial(1), 1)
+    def test_12(self):
+        self.assertEqual(factorial(12), 479001600)
     def test_0(self):
         self.assertEqual(factorial(0),1)
-    def test_values2(self):
+    def test_3(self):
         self.assertEqual(factorial(3),6)
+    def test_falsetype_str1(self):
+        self.assertRaises(TypeError, factorial, 'oooo')
+    def test_falsetype_str2(self):
+        self.assertRaises(TypeError, factorial, ' ')
+    def test_falsetype_negative(self):
+        self.assertRaises(ValueError, factorial, -15)
 
 
 if __name__ == "__main__":
