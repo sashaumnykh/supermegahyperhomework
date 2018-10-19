6135a5ba517ad731fce9aa0b022c991f0cf68c20
Aleksandra Umnykh <43292102+sashaumnykh@users.noreply.github.com>
2018-10-19 23:31:02 +0300

    READ_ME_2nd_homework_updated_version

--- 2_nd_homework/12oct_task2.py
+++ 2_nd_homework/12oct_task2.py
@@ -2,26 +2,37 @@ import unittest
 
 def perfect_number (n):
     summ_of_devisers = 0
-    for i in range (1, n):
-        if n%i == 0:
-            summ_of_devisers = summ_of_devisers + i
-    if n == summ_of_devisers:
-        return True
+    if type(n)!=int:
+        raise TypeError ("Аргумент должен быть целым числом!")
+    elif n < 0:
+        raise ValueError("Аргумент должен быть неотрицательным целым числом!")
     else:
-        return False
+        for i in range (1, n):
+            if n%i == 0:
+                summ_of_devisers = summ_of_devisers + i
+        if n == summ_of_devisers:
+            return True
+        else:
+            return False
 
 #test = int(input())
 #print (perfect_number(test))
 
 class perfect_number_test(unittest.TestCase):
-    def test_true_values(self):
+    def test_true_value1(self):
         self.assertTrue(perfect_number(6))
+    def test_true_value2(self):
         self.assertTrue(perfect_number(8128))
-
-    def test_false_values(self):
+    def test_false_value1(self):
         self.assertFalse(perfect_number(5))
+    def test_false_value2(self):
         self.assertFalse(perfect_number(127))
+    def test_false_value3(self):
         self.assertFalse(perfect_number(6324))
+    def test_falsetype_str(self):
+        self.assertRaises(TypeError, perfect_number, ' ')
+    def test_falsetype_negative(self):
+        self.assertRaises(ValueError, perfect_number, -19)
 
 if __name__ == "__main__":
     unittest.main()
--- 2_nd_homework/12oct_task3_Pascal's_triangle.py
+++ 2_nd_homework/12oct_task3_Pascal's_triangle.py
@@ -2,11 +2,16 @@ import unittest
 from math import factorial
 def pascal(n):
     a=list()
-    for i in range(n):
-        a.append(list())
-        for j in range (i+1):
-            a[i].append(int(factorial(i)/factorial(j)/factorial(i-j)))
-    return a
+    if type(n)!=int:
+        raise TypeError ("Аргумент должен быть целым числом!")
+    elif n<0:
+        raise ValueError ("Аргумент должен быть неотрицательным целым числом!")
+    else:
+        for i in range(n):
+            a.append(list())
+            for j in range (i+1):
+                a[i].append(int(factorial(i)/factorial(j)/factorial(i-j)))
+        return a
 
 #n = int(input())
 #z = pascal(n)
@@ -20,6 +25,12 @@ class pascal_test(unittest.TestCase):
         self.assertEqual(pascal(3),[[1], [1, 1], [1, 2, 1]])
     def test_values2(self):
         self.assertEqual(pascal(5),[[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]])
+    def test_falsetype_str1(self):
+        self.assertRaises(TypeError, factorial, 'aaaa')
+    def test_falsetype_str2(self):
+        self.assertRaises(TypeError, factorial, ' ')
+    def test_falsetype_negative(self):
+        self.assertRaises(ValueError, factorial, -12)
 
 if __name__ == "__main__":
     unittest.main()
