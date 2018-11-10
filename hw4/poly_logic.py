4c18e2b085455e1da4514901e803077e365887b5
Aleksandra Umnykh <43292102+sashaumnykh@users.noreply.github.com>
2018-11-10 22:18:51 +0300

    Add files via upload

---
+++ hw4/poly_logic.py
@@ -0,0 +1,42 @@
+def derivative(poly):
+    poly = poly.replace(' ', '').replace('-', '+-')
+    poly_copy = []
+    poly_copy.append('')
+    poly_copy = poly.split('+')
+    last = poly_copy[-1]
+    if last.count('x') == 0:
+        poly_copy = poly_copy[:-1]
+    index = str()
+    new_poly = str()
+    for q in range(1, len(poly_copy)):
+        part = poly_copy[q]
+        for i in range(0, len(part)):
+            if part[i] == 'x':
+                var_indexes = i
+        for i in range(0, len(part)):
+            if part[i] == '^':
+                degree_indexes = i
+            for i in range(0, len(part)):
+                if (part[i] == '-') or (part[i] == '+'):
+                    sign_indexes = i
+        if part[0] != '-':
+            for k in range(0, var_indexes):
+                index = str(index) + str(part[k])
+            index = int(index)
+            new_indexes = index * int(part[degree_indexes + 1])
+            new_degrees = (int(part[degree_indexes + 1]) - 1)
+        elif part[0] == '-':
+            for k in range(1, int(var_indexes)):
+                index = str(index) + str(part[k])
+            new_indexes = (-1) * int(index) * int(part[degree_indexes + 1])
+            new_degrees = int(part[degree_indexes + 1]) - 1
+        if new_indexes > 0:
+            new_poly += '+'
+        new_poly += str(new_indexes)
+        new_poly += 'x^'
+        new_poly += str(new_degrees)
+        index = ''
+        degree_indexes = ''
+    return new_poly
+
+print(derivative('-3x^7 + 10x^5 - 4x^2 + 3'))
