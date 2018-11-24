9d279acdfec0986353614f2d795083cf5187e830
Aleksandra Umnykh <43292102+sashaumnykh@users.noreply.github.com>
2018-11-24 20:20:32 +0300

    Update narco.py

--- hm5/task2/narco.py
+++ hm5/task2/narco.py
@@ -8,32 +8,40 @@ summary = int()
 k = int()
 buf = int()
 n1 = Narc1()
-n1.K = T1 + 1
+n1.K = T1
 n1.set_dose()
 n2 = Narc2()
 n2.wait()
 for i in range(t):
     buf += 1
-    if n1.K > 1:
+    if n1.K > 0:
         n1.K -= 1
     else:
-        if n1.condition == 'N':
-            n1.transfer()
-            n1.K = T2
-        elif n1.condition == 'p':
+        if n1.condition == 'p':
+            n1.readiness = 2
             n1.wait()
-    if n2.K > 1:
+        elif n1.condition == 'N' or n1.readiness == 1:
+            n1.readiness = 1
+            if n2.condition == '.':
+                n1.transfer()
+                n1.K = T2 - 1
+            else:
+                n1.wait()
+    if n2.K > 0:
         n2.K -= 1
     else:
         if n2.condition == 'I':
             n2.wait()
-        if n2.condition == '.' and n1.condition == '.':
+        if n2.condition == '.' and n1.condition == '.' and n1.readiness == 2:
             n2.inject()
-            n2.K = T3
+            n2.K = T3 - 1
             n1.set_dose()
-            n1.K = T1
+            n1.K = T1 - 1
             summary += buf
             k += 1
             buf = 0
+    if n1.condition == n2.condition:
+        buf -= 1
+        continue
     print(n1.condition + "  " + n2.condition)
 print(summary / k)
